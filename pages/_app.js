fecb138f9646a707b082f579779249fe3721ac33
Angela Weber <angelaweber95@hotmail.com>
2022-07-23 21:19:43 -0700

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  />;
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
